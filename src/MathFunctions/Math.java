d9beaed963e37d436d7888fcaa2d6cbff9850e70
Jerome <jeromemarco589@gmail.com>
2023-09-02 10:11:11 +0800

    Packages and Modifiers

---
+++ src/MathFunctions/Math.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MathFunctions;
+
+
+public class Math {
+ 
+    public int add (int a,int b){
+        int sum = a + b;
+        return sum;
+    }
+    
+    public int add (int a,int b,int c){
+        int sum = a + b + c;
+        return sum;
+    }
+    
+    public int substract (int a,int b){
+        int min = a - b;
+        return min;
+    }
+    
+    public int substract (int a,int b,int c){
+        int min = a - b - c;
+        return min;
+    }
+    
+    public int multiply (int a,int b){
+        int mul = a * b;
+        return mul;
+    }
+    
+    public int multiply (int a,int b,int c){
+        int mul = a * b *c;
+        return mul;
+    }
+    
+    public int divide (int a,int b){
+        int div = a / b;
+        return div;
+    }
+    
+    public int divide (int a,int b, int c){
+        int div = a / b / c;
+        return div;
+    }
+    
+    
+    
+}
---
+++ src/MathFunctions/MathConstants.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MathFunctions;
+
+/**
+ *
+ * @author JeromeMarco
+ */
+public class MathConstants {
+    
+    public double pi = 3.14;
+}
